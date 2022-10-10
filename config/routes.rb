c62db81c98b9d8eda186367dcb912c46938e7e84
Chris Siku <chrissiku5@gmail.com>
2022-10-10 15:40:54 +0200

    Fix typos errors

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
     resources :entities, only: [:index, :new, :create]
   end
   root to: 'group#index'
+
+  devise_scope  :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
 end
