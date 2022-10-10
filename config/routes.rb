c6dae50e0b5abb08688480af10a93ce4e80a23d5
Chris Siku <chrissiku5@gmail.com>
2022-10-10 13:53:51 +0200

    Create group_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  get 'group/index'
+  get 'group/new'
+  get 'group/create'
+  get 'group/group_params'
+  get 'entities/index'
+  get 'entities/new'
   devise_for :users
   # root to: "user#index"
   
